f820e0a1e36ff372e6ead4ed2be2b6323c4d09bf
zizoumunoz <zmunoz@outlook.es>
2025-11-30 14:43:04 -0800

    Add CharacterCreation Scene

---
+++ Scenes/CharacterCreation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VGP133_Final_Assignment.Scenes
+{
+    public class CharacterCreation : Scene
+    {
+        public CharacterCreation()
+        {
+            SceneName = "Character Creation";
+        }
+
+        public override void Draw()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
