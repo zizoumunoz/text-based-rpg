9aaf3f5eb997c4331adde8978f0366affdb64587
zizoumunoz <zmunoz@outlook.es>
2025-11-30 14:42:46 -0800

    Add abstract Scene class

---
+++ Scenes/Scene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VGP133_Final_Assignment.Scenes
+{
+    public abstract class Scene
+    {
+        public abstract void Update();
+        public abstract void Draw();
+
+        private string? _sceneName;
+
+        protected string? SceneName { get => _sceneName; set => _sceneName = value; }
+    }
+}
