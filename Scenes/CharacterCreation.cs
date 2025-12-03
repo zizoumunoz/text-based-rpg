e36e4609ec321754822abab39c93b1ff66a2342e
zizoumunoz <zmunoz@outlook.es>
2025-12-03 15:21:57 -0800

    Implement circle button class

--- Scenes/CharacterCreation.cs
+++ Scenes/CharacterCreation.cs
@@ -21,17 +21,27 @@ namespace VGP133_Final_Assignment.Scenes
         public override void Update()
         {
             _nameBox.Update();
+            _hairButtonA.Update();
+
+
+            // reset buttons
+            _hairButtonA.IsPressed = false;
         }
 
         public override void Render()
         {
             Raylib.ClearBackground(Color.RayWhite);
+
             _background.Render();
+
             Raylib.DrawText("Character Creation Scene", 0, 0, 20, Color.Black);
+
             _nameBox.Render();
+            _hairButtonA.Render();
         }
 
         private TextInput _nameBox = new TextInput(540, 51, 248, 112);
+        private ButtonCircle _hairButtonA = new ButtonCircle(61, 370, 350);
 
     }
 }
