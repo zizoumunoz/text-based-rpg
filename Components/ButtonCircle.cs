8bbcf4fa6dce4b40809b850f1b20e7779aabcfc7
zizoumunoz <zmunoz@outlook.es>
2025-12-03 15:21:49 -0800

    Handle clicking and hovering

--- Components/ButtonCircle.cs
+++ Components/ButtonCircle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 using Raylib_cs;
 
@@ -14,15 +15,41 @@ namespace VGP133_Final_Assignment.Components
             _yCoordinate = yCoordinate;
         }
 
-        
+        public void Update()
+        {
+            if (Raylib.CheckCollisionPointCircle(
+                    Raylib.GetMousePosition(),
+                    new Vector2(_xCoordinate, _yCoordinate),
+                    _radius))
+            {
+                _isMouseColliding = true;
+            }
+            else
+            {
+                _isMouseColliding = false;
+            }
 
+            if (_isMouseColliding && Raylib.IsMouseButtonPressed(MouseButton.Left))
+            {
+                Console.WriteLine("Clicked circle");
+                _isPressed = true;
+            }
+        }
+
+        public void Render()
+        {
+            Raylib.DrawCircle(_xCoordinate, _yCoordinate, _radius, new Color(255, 0, 0, 100));
+        }
 
         private float _radius;
         private int _xCoordinate;
         private int _yCoordinate;
+        private bool _isMouseColliding;
+        private bool _isPressed = false;
 
         public float Radius { get => _radius; set => _radius = value; }
         public int XCoordinate { get => _xCoordinate; set => _xCoordinate = value; }
         public int YCoordinate { get => _yCoordinate; set => _yCoordinate = value; }
+        public bool IsPressed { get => _isPressed; set => _isPressed = value; }
     }
 }
