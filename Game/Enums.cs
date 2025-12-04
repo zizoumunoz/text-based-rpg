b85dce7c8d1db93a9f3f21fb32169e994c9b806a
zizoumunoz <zmunoz@outlook.es>
2025-12-04 14:26:17 -0800

    Add player Class enum

--- Game/Enums.cs
+++ Game/Enums.cs
@@ -4,24 +4,31 @@ using System.Text;
 
 namespace VGP133_Final_Assignment.Game
 {
-        public enum HairColor
-        {
-            Pink,
-            Yellow,
-            Blue
-        }
+    public enum HairColor
+    {
+        Pink,
+        Yellow,
+        Blue
+    }
 
-        public enum Gender
-        {
-            Masc,
-            Other,
-            Fem
-        }
+    public enum Gender
+    {
+        Masc,
+        Other,
+        Fem
+    }
 
-        public enum Age
-        {
-            Young,
-            Adult,
-            Old
-        }
+    public enum Age
+    {
+        Young,
+        Adult,
+        Old
+    }
+
+    public enum Class
+    {
+        Knight,
+        Jester,
+        Wizard
+    }
 }
