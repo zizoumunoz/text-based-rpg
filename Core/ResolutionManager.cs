a1bfd9799425d56ba5fd2415e53055564ad6578f
zizoumunoz <zmunoz@outlook.es>
2025-12-07 16:08:15 -0800

    Add UIScale property to Resolution Manager

--- Core/ResolutionManager.cs
+++ Core/ResolutionManager.cs
@@ -8,12 +8,13 @@ namespace VGP133_Final_Assignment.Core
     {
         public static void SetResolution()
         {
-
+            
         }
 
         private static int s_virtualWidth = 384;
         private static int s_virtualHeight = 216;
 
         private static int s_uiScale = 5;
+        public static int UIScale { get => s_uiScale; set => s_uiScale = value; }
     }
 }
