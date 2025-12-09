04826195313b0aaad22e24bfc7d9f3b1d8677c9f
zizoumunoz <zmunoz@outlook.es>
2025-12-08 19:15:48 -0800

    Add monster base class

---
+++ Game/Monster.cs
@@ -0,0 +1,37 @@
+﻿using VGP133_Final_Assignment.Interfaces;
+
+namespace VGP133_Final_Assignment.Game
+{
+    public abstract class Monster : IDrawable
+    {
+        public void Render()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BasicAttack()
+        {
+
+        }
+
+        public void SpecialAttack()
+        {
+
+        }
+
+        // Monster Attributes
+        private string _name = "";
+        private int _atk;
+        private int _def;
+        private int _hp;
+        private int _goldDropped;
+        private int _xpDropped;
+
+        private float _specialAtkChance;
+    }
+}
