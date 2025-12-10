803637c4a7a7af755c73b975164667063713a939
zizoumunoz <zmunoz@outlook.es>
2025-12-09 22:37:01 -0800

    Add Terrain base class to store map data

---
+++ Game/Terrain.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+using VGP133_Final_Assignment.Components;
+using VGP133_Final_Assignment.Interfaces;
+
+namespace VGP133_Final_Assignment.Game
+{
+    public abstract class Terrain : IDrawable
+    {
+        public abstract void Render();
+
+        public abstract void Update();
+
+        protected string _name = "";
+        protected Vector2 _location;
+        protected Sprite? _sprite;
+        protected List<Monster>? _monsterPool;
+
+    }
+}
