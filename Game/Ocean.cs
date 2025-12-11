131f42cd412691a347126fae88e3ce50138bd84b
zizoumunoz <zmunoz@outlook.es>
2025-12-10 16:15:35 -0800

    Add Ocean Terrain class

---
+++ Game/Ocean.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using VGP133_Final_Assignment.Components;
+
+namespace VGP133_Final_Assignment.Game
+{
+    public class Ocean : Terrain
+    {
+        public Ocean(Vector2 location, List<Monster>? monsterPool) :
+            base(location, monsterPool)
+        {
+            _name = "Ocean";
+            _location = location;
+            _sprite = new Sprite("terrain_ocean", _location);
+        }
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Render()
+        {
+            _sprite.Render();
+        }
+    }
+}
